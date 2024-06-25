b308c58c6f05cc3b30a8e99868f024eecdd208d6
mhlangathabile <mhlangathabile27@gmail.com>
2024-06-25 19:15:12 +0200

    React Router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import "./App.css";
+import HomePage from "./HomePage";
 import { BroswerRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Bakery</h1>
-      <footer>
-        This project was coded by Thabile P Mhlanga and is{" "}
-        <a
-          href="https://github.com/mhlangathabile/react-weather-forecast"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="Content">
+          <Switch>
+            <Route path="/">
+              <HomePage />
+            </Route>
+          </Switch>
+        </div>
+        <h1>React Bakery</h1>
+        <footer>
+          This project was coded by Thabile P Mhlanga and is{" "}
+          <a
+            href="https://github.com/mhlangathabile/react-weather-forecast"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
