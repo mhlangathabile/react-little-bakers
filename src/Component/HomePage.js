e6f3a66ae0f6cad1124818098c584b305863b233
mhlangathabile <mhlangathabile27@gmail.com>
2024-06-26 21:06:30 +0200

    Navigation Bar

--- src/Component/HomePage.js
+++ src/Component/HomePage.js
@@ -3,7 +3,28 @@ import React from "react";
 const HomePage = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <nav className="nav">
+        <a href="/" className="site-name">
+          TINY BAKERS
+        </a>
+        <ul>
+          <li>
+            <a href="/">HOMEPAGE</a>
+          </li>
+          <li>
+            <a href="/AboutUs">ABOUT US</a>
+          </li>
+          <li>
+            <a href="/MenuPage">MENU </a>
+          </li>
+          <li>
+            <a href="/EventsPage">EVENTS</a>
+          </li>
+          <li>
+            <a href="/ContactUs">CONTACT US</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
