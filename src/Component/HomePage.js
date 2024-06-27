72ecde04b8c56bd4e606cba28c9c9a875197e859
mhlangathabile <mhlangathabile27@gmail.com>
2024-06-27 23:07:06 +0200

    Background-image

--- src/Component/HomePage.js
+++ src/Component/HomePage.js
@@ -1,33 +1,12 @@
 import React from "react";
 import "./HomePage.css";
+import NavBar from "./NavBar";
 
 const HomePage = () => {
   return (
-    <div>
-      {/* Navigation */}
-      <nav className="nav">
-        <a href="/" className="site-name">
-          TINY BAKERS
-        </a>
-        <ul>
-          <li>
-            <a href="/">HOMEPAGE</a>
-          </li>
-          <li>
-            <a href="/AboutUs">ABOUT US</a>
-          </li>
-          <li>
-            <a href="/MenuPage">MENU </a>
-          </li>
-          <li>
-            <a href="/EventsPage">EVENTS</a>
-          </li>
-          <li>
-            <a href="/ContactUs">CONTACT US</a>
-          </li>
-        </ul>
-      </nav>
-      <div className="homepage">
+    <div className="homepage">
+      <NavBar />
+      <div>
         <h1>TINY BAKERS</h1>
         <h3>"Sweet Treats Made by Tiny Feet!"</h3>
       </div>
