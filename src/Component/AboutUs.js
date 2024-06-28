d61b2e33f4b318941f221f6768425ea7e7e014c7
mhlangathabile <mhlangathabile27@gmail.com>
2024-06-28 08:48:49 +0200

    About Us basics

--- src/Component/AboutUs.js
+++ src/Component/AboutUs.js
@@ -1,17 +1,32 @@
 import React from "react";
 import NavBar from "./NavBar";
+import littleChefsImage from "../Images/Little Chefs.jpg";
 
 const AboutUs = () => {
   return (
     <div>
       <NavBar />
-      <div className="container">
-        <div className="row">
-          <div className="col-6">
-            <img src="/src/Images/Little Chefs.jpg" />
-          </div>
-          <div className="col-6">
-            <h1>ABOUT US</h1>
+      <div className="row p-5">
+        <div className="col-6">
+          <img
+            src={littleChefsImage}
+            alt="Little Chefs"
+            className="img-fluid"
+          />
+        </div>
+        <div className="col-6">
+          <h1 className="text-center">ABOUT US</h1>
+          <div>
+            <p>
+              Welcome to Tiny Bakers, where little hands create big delights!
+              Born from the joy of seeing kids turn kitchen messes into magical
+              treats, we are dedicated to bringing families together through the
+              art of baking. Our mission is to inspire creativity, promote
+              togetherness, and build confidence in every young baker. We value
+              fun, learning, safety, and community, ensuring that every baking
+              adventure is filled with laughter, love, and delicious memories.
+              Join us at Tiny Bakers, where every moment is a sweet celebration!
+            </p>
           </div>
         </div>
       </div>
