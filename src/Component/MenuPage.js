2a4f29254b5bfd53fc3fe27d30f15b3c1ed85e39
mhlangathabile <mhlangathabile27@gmail.com>
2024-06-29 23:16:02 +0200

    Menu basic structure

--- src/Component/MenuPage.js
+++ src/Component/MenuPage.js
@@ -2,9 +2,52 @@ import React from "react";
 import NavBar from "./NavBar";
 
 const MenuPage = () => {
+  let Menu = [
+    {
+      name: "Nutella Heart Pie Pops",
+      price: "R25",
+    },
+    {
+      name: "Popsicles",
+      price: "R10",
+    },
+    {
+      name: "Another Item",
+      price: "R30",
+    },
+  ];
+
   return (
     <div>
       <NavBar />
+      <h1 className="text-center p-3">MENU</h1>
+      <div className="row">
+        {Menu.map((item, index) => (
+          <div className="col-4" key={index}>
+            <p>
+              {item.name} {item.price}
+            </p>
+          </div>
+        ))}
+      </div>
+      <div className="row">
+        {Menu.map((item, index) => (
+          <div className="col-4" key={index}>
+            <p>
+              {item.name} {item.price}
+            </p>
+          </div>
+        ))}
+      </div>
+      <div className="row">
+        {Menu.map((item, index) => (
+          <div className="col-4" key={index}>
+            <p>
+              {item.name} {item.price}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
