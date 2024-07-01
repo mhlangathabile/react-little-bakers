95e76bf55cb15df1d070939a8f81728d56ff6c6f
mhlangathabile <mhlangathabile27@gmail.com>
2024-07-02 00:15:27 +0200

    ContactUs Component

--- src/Component/ContactUs.js
+++ src/Component/ContactUs.js
@@ -9,47 +9,34 @@ const ContactUs = () => {
         <div class="row align-items-center g-lg-5 py-5">
           <div class="col-lg-7 text-center text-lg-start">
             <h1 class="display-4 fw-bold lh-1 text-body-emphasis mb-3">
-              Vertically centered hero sign-up form
+              Contact Our Baking Team!
             </h1>
             <p class="col-lg-10 fs-4">
-              Below is an example form built entirely with Bootstrap’s form
-              controls. Each required form group has a validation state that can
-              be triggered by attempting to submit the form without completing
-              it.
+              We'd love to hear from you! Whether you have questions,
+              suggestions, or just want to share your baking adventures, we're
+              here to help. Reach out to us, and our friendly team at Tiny
+              Bakers will get back to you as soon as possible. Let's make sweet
+              memories together!
             </p>
           </div>
           <div class="col-md-10 mx-auto col-lg-5">
             <form class="p-4 p-md-5 border rounded-3 bg-body-tertiary">
               <div class="form-floating mb-3">
-                <input
-                  type="email"
-                  class="form-control"
-                  id="floatingInput"
-                  placeholder="name@example.com"
-                />
-                <label for="floatingInput">Email address</label>
+                <input type="text" class="form-control" id="floatingInput" />
+                <label for="floatingInput">Name</label>
               </div>
               <div class="form-floating mb-3">
                 <input
-                  type="password"
+                  type="textarea"
                   class="form-control"
                   id="floatingPassword"
                   placeholder="Password"
                 />
-                <label for="floatingPassword">Password</label>
-              </div>
-              <div class="checkbox mb-3">
-                <label>
-                  <input type="checkbox" value="remember-me" /> Remember me
-                </label>
+                <label for="floatingPassword">Dear Bakers...</label>
               </div>
               <button class="w-100 btn btn-lg btn-primary" type="submit">
-                Sign up
+                Let's Connect
               </button>
-              <hr class="my-4" />
-              <small class="text-body-secondary">
-                By clicking Sign up, you agree to the terms of use.
-              </small>
             </form>
           </div>
         </div>
