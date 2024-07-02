31fa62533b373cedf43130b44371d8a219ba678f
mhlangathabile <mhlangathabile27@gmail.com>
2024-07-02 12:12:10 +0200

    Homepage structure

--- src/Component/HomePage.js
+++ src/Component/HomePage.js
@@ -1,14 +1,24 @@
 import React from "react";
 import "./HomePage.css";
 import NavBar from "./NavBar";
+import DonutsGif from "../Images/Donuts .gif";
 
 const HomePage = () => {
   return (
     <div>
       <NavBar />
-      <div className="homepage">
-        <h1>TINY BAKERS</h1>
-        <h3>"Sweet Treats Made by Tiny Feet!"</h3>
+      <div className="homepage row">
+        <div className="col-6">
+          <img
+            src={DonutsGif}
+            alt="DonutsGif"
+            style={{ height: "100%", width: "100%" }}
+          />
+        </div>
+        <div className="col-6">
+          <h1>TINY BAKERS</h1>
+          <h3>"Sweet Treats Made by Tiny Feet!"</h3>
+        </div>
       </div>
     </div>
   );
