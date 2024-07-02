fa4a16be48e2a5e7c8a9cde3b5f3afa9b148d1b7
mhlangathabile <mhlangathabile27@gmail.com>
2024-07-02 14:42:33 +0200

    Contact us background image

--- src/Component/ContactUs.js
+++ src/Component/ContactUs.js
@@ -1,13 +1,12 @@
 import React from "react";
 import NavBar from "./NavBar";
 import "./ContactUs.css";
-import contactImage from "../Images/contactUs.jpg";
 
 const ContactUs = () => {
   return (
     <div className="contactUs">
       <NavBar />
-      <div class="container col-xl-10 col-xxl-8 px-4 py-5 contactUs">
+      <div class="container col-xl-10 col-xxl-8 px-4 py-5">
         <div class="row align-items-center g-lg-5 py-5">
           <div class="col-lg-7 text-center text-lg-start">
             <h1 class="display-4 fw-bold lh-1 text-body-emphasis mb-3">
