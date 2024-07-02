8abe8905c85db8d1291ab36eeb39d5a66fe082a3
mhlangathabile <mhlangathabile27@gmail.com>
2024-07-02 13:03:03 +0200

    About Us Css

--- src/Component/AboutUs.js
+++ src/Component/AboutUs.js
@@ -1,22 +1,16 @@
 import React from "react";
 import NavBar from "./NavBar";
-import littleChefsImage from "../Images/Little Chefs.jpg";
+import "./AboutUs.css";
+import aboutUsImage from "../Images/Still Life Image.gif";
 
 const AboutUs = () => {
   return (
     <div>
       <NavBar />
-      <div className="row p-5">
-        <div className="col-6">
-          <img
-            src={littleChefsImage}
-            alt="Little Chefs"
-            className="img-fluid"
-          />
-        </div>
+      <div className="row p-5 aboutUs">
         <div className="col-6">
           <h1 className="text-center">ABOUT US</h1>
-          <div className=" border-top border-right border-danger rounded-bottom p-5">
+          <div className=" border-top border-right border-light rounded-bottom p-5">
             <p>
               Welcome to Tiny Bakers, where little hands create big delights!
               Born from the joy of seeing kids turn kitchen messes into magical
@@ -28,6 +22,9 @@ const AboutUs = () => {
               Join us at Tiny Bakers, where every moment is a sweet celebration!
             </p>
           </div>
+        </div>
+        <div className="col-6">
+          <img src={aboutUsImage} alt="Little Chefs" className="img-fluid" />
         </div>
       </div>
     </div>
