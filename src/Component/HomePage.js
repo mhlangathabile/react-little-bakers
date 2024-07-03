55fd90c655df8d635ffce04a48cd6d298688afe2
mhlangathabile <mhlangathabile27@gmail.com>
2024-07-03 23:59:22 +0200

    CSS

--- src/Component/HomePage.js
+++ src/Component/HomePage.js
@@ -1,26 +1,15 @@
 import React from "react";
 import "./HomePage.css";
 import NavBar from "./NavBar";
-import ChefsAttire from "../Images/Chefs attire.jpg";
+import BakersShop from "../Images/Bakers shop.jpg";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="homepage">
       <NavBar />
-      <div className="homepage row">
-        <div className="col-6 bakers">
-          <h1>TINY BAKERS</h1>
-          <br />
-          <h3>"Sweet Treats Made by Tiny Feet!"</h3>
-        </div>
-        <div className="col-6">
-          <img
-            src={ChefsAttire}
-            alt="DonutsGif"
-            // style={{ height: "100%", width: "100%" }}
-          />
-        </div>
-      </div>
+      <h1>TINY BAKERS</h1>
+      <img src={BakersShop} />
+      <h3>"SWEET TREATS MADE BY TINY FEET!"</h3>
     </div>
   );
 };
