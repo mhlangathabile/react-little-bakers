e1fef5eedcf07f6c99cb0047313b021a2804ddf4
mhlangathabile <mhlangathabile27@gmail.com>
2024-07-10 15:37:09 +0200

    Update HomePage.js

--- src/Component/HomePage.js
+++ src/Component/HomePage.js
@@ -7,9 +7,6 @@ const HomePage = () => {
   return (
     <div className="homepage">
       <NavBar />
-      <h1>TINY BAKERS</h1>
-      <img src={BakersShop} />
-      <h3>"SWEET TREATS MADE BY TINY FEET!"</h3>
     </div>
   );
 };
